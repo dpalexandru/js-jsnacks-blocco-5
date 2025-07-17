4f96f1c60bbbbbb8849dba33e32d4dfb75c543d4
dpalexandru <alexpoian91@hotmail.com>
2025-07-17 16:00:59 +0200

    feat: snak-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,27 @@
-const students = [
-  {
-    id: 1,
-    name: 'Marco Lanci',
-    age: 32,
-    class: '3C'
-  },
-  {
-    id: 2,
-    name: 'Mario Banfi',
-    age: 34,
-    class: '4A'
-  },
-  {
-    id: 3,
-    name: 'Luigi Banzi',
-    age: 33,
-    class: '5B'
-  },
+const students = [{
+        id: 1,
+        name: 'Marco Lanci',
+        age: 32,
+        class: '3C'
+    },
+    {
+        id: 2,
+        name: 'Mario Banfi',
+        age: 34,
+        class: '4A'
+    },
+    {
+        id: 3,
+        name: 'Luigi Banzi',
+        age: 33,
+        class: '5B'
+    },
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const oggettoMarcoLanci = students.find(result => result.name === 'Marco Lanci');
+const { class: classe, name } = oggettoMarcoLanci;
+
+console.log(classe);
