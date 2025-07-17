0cc8dc32a49a172eaf275f34978d4c65b1fd182b
dpalexandru <alexpoian91@hotmail.com>
2025-07-17 15:20:30 +0200

    feat: snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,8 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const nuoviNumeri = numbers.map(numero => numero = numero + 1);
+
+console.log(numbers);
+console.log(nuoviNumeri);
