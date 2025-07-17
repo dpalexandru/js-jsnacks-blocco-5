bf5cc43889a6149ae23b37fc86ae4e4547a17fe8
dpalexandru <alexpoian91@hotmail.com>
2025-07-17 15:49:24 +0200

    feat: snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,23 +1,25 @@
-const students = [
-  {
-    id: 1,
-    name: 'Marco Lanci',
-    age: 32,
-    class: '3C'
-  },
-  {
-    id: 2,
-    name: 'Mario Banfi',
-    age: 34,
-    class: '4A'
-  },
-  {
-    id: 3,
-    name: 'Luigi Banzi',
-    age: 33,
-    class: '5B'
-  },
+const students = [{
+        id: 1,
+        name: 'Marco Lanci',
+        age: 32,
+        class: '3C'
+    },
+    {
+        id: 2,
+        name: 'Mario Banfi',
+        age: 34,
+        class: '4A'
+    },
+    {
+        id: 3,
+        name: 'Luigi Banzi',
+        age: 33,
+        class: '5B'
+    },
 ];
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const risultatoCercato = students.find(result => result.id === 2);
+console.log(risultatoCercato);
